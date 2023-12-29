11643ed6a746d86562a015a3027056a81ec97b78
Sergey Lystin <slustin92@mail.ru>
2023-12-30 01:12:34 +0400

    Добавил печать массива

--- Program.cs
+++ Program.cs
@@ -13,3 +13,13 @@ void SortArray(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SortArray(array1, array2);
+PrintArray(array2);
