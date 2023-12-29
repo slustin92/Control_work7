af5979628a9768ccd6ba6e09593a2bb68f870a66
Sergey Lystin <slustin92@mail.ru>
2023-12-30 01:10:35 +0400

    Создал метод проверки

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] array1 = new string[4] {"1234", "1567", "-2", "computer science"};
 string[] array2 = new string[array1.Length];
+
+void SortArray(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[j] = array1[i];
+        j++;
+        }
+    }
+}
